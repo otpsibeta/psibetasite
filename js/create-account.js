7b1bea9d508559ce13445386e71a706aae37b571
Yash Parekh <yparekh39@gmail.com>
2015-06-30 21:38:28 -0500

    Creates base data when account created

--- js/create-account.js
+++ js/create-account.js
@@ -1,6 +1,7 @@
 $("#create").on("click", function(){
 	var email = $("#email").val();
 	var pass = $("#password").val();
+	var name = $("#name").val();
 	if(pass !== $("#confirm").val()){
 		alert("Password and Confirm Password are not the same");
 		location.reload();
@@ -12,10 +13,26 @@ $("#create").on("click", function(){
 	}, function(error, userData) {
 	  if (error) {
 	    console.log("Error creating user:", error);
+	    alert("Error creating user:", error);
 	  } else {
-	    //console.log("Successfully created user account with uid:", userData.uid);
-	  	alert("Successfully created user account!");
-	  	location.reload();
+		  	var url = "https://torrid-heat-3899.firebaseIO.com/users/" + userData.uid;
+		  	var userRef = new Firebase(url);
+		    console.log("Successfully created user account with uid:", userData.uid);
+		    userRef.transaction(function(currentData){
+		    	if(currentData === null){
+		    		currentData = 
+			    	{ 
+				    	fullName: name,
+				    	paidDues: "Not Yet",
+				    	userEmail: email
+			    	}
+		    	}
+				return currentData;
+		    }, function(){
+		    	console.log("created data");
+		    });
+		  	alert("Successfully created user account!");
+		  	//location.reload();
 	  }
 	});
 });
