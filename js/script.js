dd34eb755845cec6315f94fb0a79bf603b6bc65b
Luis Petitjean <petitjean.luis@gmail.com>
2018-01-17 18:38:09 -0600

    added responsive link

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/**
+ * Determine the mobile operating system.
+ * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+ *
+ * @returns {String}
+ */
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "windows";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "ios";
+    }
+
+    return "unknown";
+}
+
+var os = getMobileOperatingSystem();
+if (os === "android") {
+    $('.fb').attr("href","hello");
+    $('.ig').attr("href","hello");
+    $('.tt').attr("href","hello");
+}else if (os === "ios") {
+    $('.fb').attr("href","fb://page/ThetaTauSpringRush2018");
+    $('.ig').attr("href","https://www.instagram.com/texasthetatau/");
+    $('.tt').attr("href","https://twitter.com/TexasThetaTau");
+} else {
+    $('.fb').attr("href","https://www.facebook.com/groups/ThetaTauSpringRush2018/");
+    $('.ig').attr("href","https://www.instagram.com/texasthetatau/");
+    $('.tt').attr("href","https://twitter.com/TexasThetaTau");
+}
